ebebba289d362aadd7018ff1df6d9b71a9177e91
Anthony Aloisio <Z1927201@students.niu.edu>
2024-08-08 13:22:26 -0500

    fix password reset timer

--- src/public/resetPassword.php
+++ src/public/resetPassword.php
@@ -7,21 +7,7 @@ if(!isset($_GET["token"])){
 }
 session_destroy();
 
-// connect to db
-include "../api/database.php";
-if(mysqli_connect_errno()) {
-    header("Location: login.php?connection=failed");
-}
-
-
-$result = mysqli_query($connection, "SELECT token FROM ChangePass WHERE time_created > DATE_SUB(NOW(), INTERVAL 10 MINUTE) LIMIT 1");
-
-// make sure token hasn't expired
-if(mysqli_num_rows($result) < 1){
-    header("Location: login.php?token=error");
-}
-
-$token = mysqli_fetch_assoc($result)["token"];
+include '../api/getResetToken.php';
 
 // make sure token matches
 if($token != $_GET["token"]){
